5d7ce3276afa8ded7ce840d561e9f2ac1021edb0
Gustavo Henrique <gustavojs417@gmail.com>
2021-12-31 17:30:29 -0300

    update files

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -38,6 +38,60 @@ const reviews = [
   },
 ];
 
+// select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector(".nex-btn");
+const randomBtn = document.querySelector('.random-btn');
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function() {
+  showPerson(currentItem);
+});
+
+// the function of show person
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  info.textContent = item.text;
+  job.textContent = item.job;
+}
+
+// show person next
+nextBtn.addEventListener("click", function() {
+  currentItem++;
+  if (currentItem > reviews - 1) {
+    currentItem = 0;
+  }
+
+  showPerson(currentItem);
+});
+
+// show prev person
+prevBtn.addEventListener("click", function() {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+
+  showPerson(currentItem);
+});
+
+// show random person
+randomBtn.addEventListener("click", function() {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+});
+
+
 // ╔╗
 // ║║╔═╦╦╦═╗
 // ║╚╣║║║║╩╣
